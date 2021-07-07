011833b62e1a3ae753eb0ba9a61dd00ab167c96a
Rishikesh Shukla <rishi1160@gmail.com>
2021-07-07 15:26:58 -0700

    add views

--- PitchPerfect/AppDelegate.swift
+++ PitchPerfect/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("app started")
         return true
     }
 
--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("clicked")
+        recordingLabel.text = "recording"
+    }
 }
 
