16a2744623591c4dc2780fd3624626ab663bbe06
Rishikesh Shukla <rishi1160@gmail.com>
2021-07-27 12:38:02 -0700

    finish lesson 3

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("View did load")
+        stopRecordingButton.isEnabled = false
     }
 
     @IBAction func recordAudio(_ sender: Any) {
         print("clicked")
-        recordingLabel.text = "recording"
+        recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print("View Will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("View Did Appear")
+    }
+    
 }
 
